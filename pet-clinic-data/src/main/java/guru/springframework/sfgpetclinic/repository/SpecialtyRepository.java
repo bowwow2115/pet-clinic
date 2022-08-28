9e24008dcb61917f5ce000051ec1f3e66c1a3278
bowwow2115 <bowwow2115@gmail.com>
2022-08-28 22:18:23 +0900

    Repository 인터페이스 추가

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/SpecialtyRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.repository;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialtyRepository extends CrudRepository<Speciality, Long> {
+
+}
