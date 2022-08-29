a77fa292e1d51cfbac1d1bfad23e1cf3426df2cc
bowwow2115 <bowwow2115@gmail.com>
2022-08-30 00:28:53 +0900

    JPA 서비스 비즈니스로직 추가

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/PetRepository.java
@@ -1,7 +1,8 @@
 package guru.springframework.sfgpetclinic.repository;
 
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetRepository extends CrudRepository<PetType, Long> {
+public interface PetRepository extends CrudRepository<Pet, Long> {
 }
